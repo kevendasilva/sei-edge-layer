c0a471e3aa81aadff24f921d6bafbc5ef3d947c8
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-06-02 11:29:46 -0300

    feat: connection to broker

--- sensor-node/sensor-node.ino
+++ sensor-node/sensor-node.ino
@@ -4,9 +4,12 @@
 
 #include <ArduinoJson.h>
 #include "FS.h"
+#include <PubSubClient.h>
 
 // Constants for ESP8266
 #if defined(ESP8266)
+  #define BOARD_NAME "ESP8266"
+
   #define FAIL_LED_PIN D0
   #define SUCCESS_LED_PIN D1
 #endif
@@ -26,6 +29,13 @@ const char *mqttPassword;
 
 // Node ID
 short nodeID;
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Topic
+String topicBarrierState;
 
 void setup() {
   Serial.begin(115200);
@@ -54,6 +64,9 @@ void setup() {
   }
 
   connectToWiFi();
+  connectToBroker();
+
+  subscribeTopics();
 }
 
 void loop() {
@@ -62,7 +75,20 @@ void loop() {
     return;
   }
 
+  if (!client.connected()) {
+    ESP.restart();
+  }
+
   turnOnLED(FAIL_LED_PIN);
+
+  client.loop();
+}
+
+void openBarrier() {
+  turnOffLED(FAIL_LED_PIN);
+  turnOnLED(SUCCESS_LED_PIN);
+  delay(3000);
+  turnOffLED(SUCCESS_LED_PIN);
 }
 
 void turnOnLED(unsigned short pin) { digitalWrite(pin, HIGH); }
@@ -121,6 +147,59 @@ bool loadConfig() {
   configFile.close();
 
   return true;
+}
+
+void subscribeTopics() {
+  // Defining the topic to handle the barrier state
+  topicBarrierState = "barrier/" + String(nodeID) + "/state";
+
+  // Subscribe topic 'barrier/nodeID/state'
+  client.subscribe(topicBarrierState.c_str());
+}
+
+// Callback for new messages
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message: ");
+
+  String message = "";
+
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  Serial.print(message);
+
+  // Open barrier
+  if (String(topic) == topicBarrierState) {
+    if (message == "open") {
+      openBarrier();
+    }
+  }
+
+  Serial.println();
+}
+
+void connectToBroker() {  
+  client.setServer(mqttBroker, mqttPort);
+  client.setCallback(callback);
+
+  while (!client.connected()) {    
+    String clientId = String(BOARD_NAME) + "-#" + String(nodeID);
+
+    Serial.printf("The client %s connects to Mosquitto MQTT broker.\n", clientId.c_str());
+
+    if (client.connect(clientId.c_str(), mqttUsername, mqttPassword)) {
+      Serial.println("Connected to the Mosquitto MQTT broker.");
+    } else {
+      Serial.print("Failed with state ");
+      Serial.print(client.state());
+      Serial.println();
+
+      loadingSignal();
+    }
+  }
 }
 
 void connectToWiFi() {
